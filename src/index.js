72f0dabb920fe2745c3360e6806de1247de04f12
Denis <denis97@wp.pl>
2023-10-25 16:29:55 +0200

    commit fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -7,14 +7,15 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.querySelector('#root')
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
 );
 
-
-
+root.render(
+  
+      <Provider store={store}>
+        <App />
+      </Provider>
+  
+  );
