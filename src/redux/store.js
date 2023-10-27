9bee962b26c4e0441e7ddce2db04176f30eb103e
Denis <denis97@wp.pl>
2023-10-27 17:46:50 +0200

    Favourite

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,10 @@ import { createStore } from 'redux';
 import initialState from './initialState';
 import shortid from 'shortid';
 import { strContains } from '../utils/strContains';
-// Selector
+
 export const getFilteredCards = ({ cards, searchString }, columnId) =>
-  //data destructuring, extracting values ​​to const...
-  cards.filter(
+
+cards.filter(
     (card) =>
       card.columnId === columnId && strContains(card.title, searchString)
   );
@@ -22,7 +22,9 @@ export const getAllLists = (state) => state.lists;
 
 export const getSearchString = (state) => state.searchString;
 
-// action creators
+export const getFavoriteCards = ({ cards }) =>
+  cards.filter((card) => card.isFavorite === true);
+
 export const addColumn = (payload) => ({ type: 'ADD_COLUMN', payload });
 
 export const addCard = (payload) => ({ type: 'ADD_CARD', payload });
@@ -33,6 +35,11 @@ export const searchString = (payload) => ({
 });
 
 export const addList = (payload) => ({ type: 'ADD_LIST', payload });
+
+export const toggleCardFavorite = (payload) => ({
+  type: 'TOGGLE_CARD_FAVORITE',
+  payload,
+});
 
 const reducer = (state, action) => {
   switch (action.type) {
@@ -60,6 +67,15 @@ const reducer = (state, action) => {
     case 'UPDATE_SEARCHSTRING': {
       return { ...state, searchString: action.payload };
     }
+    case 'TOGGLE_CARD_FAVORITE':
+      return {
+        ...state,
+        cards: state.cards.map((card) =>
+          card.id === action.payload
+            ? { ...card, isFavorite: !card.isFavorite }
+            : card
+        ),
+      };
     default:
       return state;
   }
